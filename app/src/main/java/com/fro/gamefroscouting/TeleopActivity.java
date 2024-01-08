5c6adcffe8ad54994afb656f61b7ed65f116d214
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-08 10:35:02 -0500

    Made endgame screen background

--- app/src/main/java/com/fro/gamefroscouting/TeleopActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/TeleopActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelepopActivity extends AppCompatActivity {
+public class TeleopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_telepop);
+        setContentView(R.layout.activity_teleop);
     }
 }
