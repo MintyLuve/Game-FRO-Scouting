35b3b50de2b035b5ab38ab57bd1f1b99814b17e9
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-18 17:29:45 -0500

    fixed the menu button not working, also made menu icons change when pressed (help and bars), also changed all fonts to serif

--- app/src/main/java/com/fro/gamefroscouting/ScoutingActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/ScoutingActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ public class ScoutingActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     BottomNavigationView bottomNavigationView;
     ImageButton help;
+    ImageButton menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,7 @@ public class ScoutingActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         drawerLayout = findViewById(R.id.drawer_layout);
         help = findViewById(R.id.helpButton);
+        menu = findViewById(R.id.menuButton);
 
         // switching fragments
         replaceFragment(new AutoFragment());
@@ -59,7 +62,7 @@ public class ScoutingActivity extends AppCompatActivity {
         });
         bottomNavigationView.setItemIconTintList(null);
 
-        //snackbar
+        // help snackbar
         help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +75,8 @@ public class ScoutingActivity extends AppCompatActivity {
                 //changes snackbar layout
                 Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
                 snackbarLayout.setPadding(0, 0, 0, 0);
+                help.setBackgroundResource(R.drawable.menu_help_exit);
+
 
                 View behind = mySnackbar.findViewById(R.id.behind);
                 behind.setOnClickListener(new View.OnClickListener() {
@@ -80,12 +85,66 @@ public class ScoutingActivity extends AppCompatActivity {
                         snackbar.dismiss();
                     }
                 });
-
                 // add the custom snack bar layout to snackbar layout
                 snackbarLayout.addView(mySnackbar, 0);
                 snackbar.show();
 
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        help.setBackgroundResource(R.drawable.menu_help_button);
+                    }});
+            }
+        });
 
+        // menu snackbar
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // makes a snackbar with no text
+                final Snackbar snackbar = Snackbar.make(v, "", Snackbar.LENGTH_LONG);
+                // set the background to my snackbar
+                View mySnackbar = getLayoutInflater().inflate(R.layout.menu_snackbar, null);
+                //makes background transparent so the custom view can be seen
+                snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+                //changes snackbar layout
+                Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                snackbarLayout.setPadding(0, 0, 0, 0);
+                menu.setBackgroundResource(R.drawable.menu_bars_exit);
+
+                // behind button
+                View behind = mySnackbar.findViewById(R.id.behind);
+                behind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                // back button
+                View back = mySnackbar.findViewById(R.id.returnButton);
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+                // submit button
+                View submit = mySnackbar.findViewById(R.id.submitButton);
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+                // add the custom snack bar layout to snackbar layout
+                snackbarLayout.addView(mySnackbar, 0);
+                snackbar.show();
+
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        menu.setBackgroundResource(R.drawable.menu_bars_button);
+                }});
             }
         });
     }
