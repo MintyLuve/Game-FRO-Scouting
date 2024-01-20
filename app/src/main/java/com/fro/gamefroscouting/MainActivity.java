f84104e1085cf54063d6afe02b960568410b7454
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-20 12:13:11 -0500

    wohooo main page lets go!!!!!!!!

--- app/src/main/java/com/fro/gamefroscouting/MainActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button1);
+        Spinner teamSpinner = (Spinner) findViewById(R.id.teamSpinner);
+        Spinner posSpinner = (Spinner) findViewById(R.id.posSpinner);
 
+        // changes to next page on click
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), ScoutingActivity.class));
             }
         });
+
+        // adds options to the team spinner
+        ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource
+                (this, R.array.team_numbers, android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpinner.setAdapter(teamAdapter);
+
+        // adds options to the position spinner
+        ArrayAdapter<CharSequence> posAdapter = ArrayAdapter.createFromResource
+                (this, R.array.positions, android.R.layout.simple_spinner_item);
+        posAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        posSpinner.setAdapter(posAdapter);
     }
 }
