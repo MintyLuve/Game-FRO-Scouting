5209ae654327b1a7858ade4b3db433decc0d3a16
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-20 16:13:44 -0500

    added switch and dropdown to auto

--- app/src/main/java/com/fro/gamefroscouting/AutoFragment.java
+++ app/src/main/java/com/fro/gamefroscouting/AutoFragment.java
@@ -1,18 +1,29 @@
 package com.fro.gamefroscouting;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.fro.gamefroscouting.R;
 
 public class AutoFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_auto, container, false);
+
+        Spinner posSpinner =  rootView.findViewById(R.id.posSpinner);
+
+        // adds options to the position spinner
+        ArrayAdapter<CharSequence> posAdapter = ArrayAdapter.createFromResource
+                (getActivity(), R.array.end_pos, android.R.layout.simple_spinner_item);
+        posAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        posSpinner.setAdapter(posAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auto, container, false);
+        return rootView;
     }
 }
--- app/src/main/java/com/fro/gamefroscouting/TeleopFragment.java
+++ app/src/main/java/com/fro/gamefroscouting/TeleopFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class TeleopFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
