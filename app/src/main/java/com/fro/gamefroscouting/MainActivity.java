029cc60ae6abac0677f133ee38fabd016f057735
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-26 21:06:04 -0500

    made pretty :)

--- app/src/main/java/com/fro/gamefroscouting/MainActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button plus;
-    Button minus;
+    ImageButton plus;
+    ImageButton minus;
     TextView textView;
     int textNum = 0;
     @Override
