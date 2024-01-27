53a8a029fdd614f5adb375c470312f1bd22beda1
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-01-26 20:47:54 -0500

    yippee this thing works (plus/minus view)

--- app/src/main/java/com/fro/gamefroscouting/MainActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/MainActivity.java
@@ -3,12 +3,41 @@ package com.fro.gamefroscouting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button plus;
+    Button minus;
+    TextView textView;
+    int textNum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+        textView = findViewById(R.id.number);
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textNum < 20){
+                    textNum++;
+                    textView.setText(""+textNum);
+                }            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textNum > 0){
+                    textNum--;
+                    textView.setText(""+textNum);
+                }
+            }
+        });
     }
 }
