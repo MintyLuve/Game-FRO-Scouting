8646be3a246726626a8399cc4799803789b6acb9
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-02-02 21:20:54 -0500

    fixed minus, notes titles, and added values file

---
+++ app/src/main/java/com/fro/gamefroscouting/Values.java
@@ -0,0 +1,21 @@
+package com.fro.gamefroscouting;
+
+public class Values {
+    //start
+    String start_scout_name, start_robo_pos;
+    int start_team_num, start_match_num;
+    //auto
+    String auto_end_pos;
+    int auto_co_bo, auto_co_mi, auto_co_to, auto_cu_bo, auto_cu_mi, auto_cu_to;
+    boolean auto_leave_com;
+    //teleop
+    int tele_co_bo, tele_co_mi, tele_co_to, tele_cu_bo, tele_cu_mi, tele_cu_to;
+    boolean tele_ground, tele_single, tele_double;
+    //endgame
+    String eg_end_pos;
+    //notes
+    String notes_type_box;
+    int notes_defends, notes_defended;
+    boolean notes_robo_break, notes_robo_tip, notes_penalty;
+
+}
