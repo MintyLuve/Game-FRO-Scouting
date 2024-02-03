91385e377d466159729bd334c70e89550962afe0
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-02-03 14:30:14 -0500

    added help button on main screen and added crescendo logo

--- app/src/main/java/com/fro/gamefroscouting/MainActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/MainActivity.java
@@ -2,27 +2,36 @@ package com.fro.gamefroscouting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton help;
     Button button;
+    AutoCompleteTextView teamType;
+    Spinner posSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button1);
-        AutoCompleteTextView teamType = (AutoCompleteTextView) findViewById(R.id.teamType);
-        Spinner posSpinner = (Spinner) findViewById(R.id.posSpinner);
+        button = findViewById(R.id.button1);
+        teamType = findViewById(R.id.teamType);
+        posSpinner = findViewById(R.id.posSpinner);
+        help = findViewById(R.id.helpButton);
 
         String[] teamNums = getResources().getStringArray(R.array.team_numbers);
 
@@ -45,5 +54,41 @@ public class MainActivity extends AppCompatActivity {
                 (this, R.array.positions, android.R.layout.simple_spinner_item);
         posAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         posSpinner.setAdapter(posAdapter);
+
+        // help snack bar
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // makes a snack bar with no text (length is time)
+                final Snackbar snackbar = Snackbar.make(v, "", Snackbar.LENGTH_INDEFINITE);
+                // set the background to my snack bar
+                View mySnackBar = getLayoutInflater().inflate(R.layout.help_snackbar_mainpg, null);
+                //makes background transparent so the custom view can be seen
+                snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+                //changes snack bar layout
+                @SuppressLint("RestrictedApi") Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                snackbarLayout.setPadding(0, 0, 0, 0);
+                //sets the button to have an x
+                help.setImageResource(R.drawable.menu_help_exit);
+
+                //when behind is clicked it dismisses the snack bar
+                View behind = mySnackBar.findViewById(R.id.behind);
+                behind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                // add the custom snack bar layout to snack bar layout
+                snackbarLayout.addView(mySnackBar, 0);
+                snackbar.show();
+
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        help.setImageResource(R.drawable.menu_help_button);
+                    }});
+            }
+        });
     }
 }
