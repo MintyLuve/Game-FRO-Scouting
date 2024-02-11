0682bde68a8ed67e4b04a816b876382313df7761
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-02-10 22:29:15 -0500

    added code for switching pages + error fix

--- app/src/main/java/com/fro/gamefroscouting/MainActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/MainActivity.java
@@ -2,18 +2,75 @@ package com.fro.gamefroscouting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     //declaring
-    //variable type 1
+    //button
+    TextView startButton;
+    ImageButton helpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //instantiating
-        //variable type 1
+        //button
+        startButton = findViewById(R.id.startButton);
+        helpButton = findViewById(R.id.helpButton);
+
+        // changes to next page on click
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ScoutingActivity.class));
+            }
+        });
+
+        // help snack bar
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // makes a snack bar with no text (length is time)
+                final Snackbar snackbar = Snackbar.make(v, "", Snackbar.LENGTH_INDEFINITE);
+                // set the background to my snack bar
+                View mySnackBar = getLayoutInflater().inflate(R.layout.help_snackbar_main, null);
+                //makes background transparent so the custom view can be seen
+                snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+                //changes snack bar layout
+                @SuppressLint("RestrictedApi") Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                snackbarLayout.setPadding(0, 0, 0, 0);
+                //sets the button to have an x
+                helpButton.setImageResource(R.drawable.menu_help_exit_button);
+
+                //when behind is clicked it dismisses the snack bar
+                View behind = mySnackBar.findViewById(R.id.behind);
+                behind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                // add the custom snack bar layout to snack bar layout
+                snackbarLayout.addView(mySnackBar, 0);
+                snackbar.show();
+
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        helpButton.setImageResource(R.drawable.menu_help_button);
+                    }});
+            }
+        });
 
     }
 }
--- app/src/main/java/com/fro/gamefroscouting/ScoutingActivity.java
+++ app/src/main/java/com/fro/gamefroscouting/ScoutingActivity.java
@@ -1,19 +1,151 @@
 package com.fro.gamefroscouting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ScoutingActivity extends AppCompatActivity {
     //declaring
-    //variable type 1
-
+    //bottom menu
+    BottomNavigationView menuBar;
+    //mg buttons
+    ImageButton helpButton;
+    ImageButton menuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scouting);
         //instantiating
-        //variable type 1
+        //bottom menu
+        menuBar = findViewById(R.id.menu_items);
+        //mg buttons
+        helpButton = findViewById(R.id.helpButton);
+        menuButton = findViewById(R.id.menuButton);
 
+        // switching fragments
+        replaceFragment(new AutoFragment());
+
+        menuBar.setBackground(null);
+        menuBar.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.auto_button) {
+                replaceFragment(new AutoFragment());
+            } else if (item.getItemId() == R.id.teleop_button) {
+                replaceFragment(new TeleopFragment());
+            } else if (item.getItemId() == R.id.endgame_button) {
+                replaceFragment(new EndgameFragment());
+            } else if (item.getItemId() == R.id.notes_button) {
+                replaceFragment(new NotesFragment());
+            }
+            return true;
+        });
+        //no tint on menu icons
+        menuBar.setItemIconTintList(null);
+
+        // help snack bar
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // makes a snack bar with no text (length is time)
+                final Snackbar snackbar = Snackbar.make(v, "", Snackbar.LENGTH_INDEFINITE);
+                // set the background to my snack bar
+                View mySnackBar = getLayoutInflater().inflate(R.layout.help_snackbar_scouting, null);
+                //makes background transparent so the custom view can be seen
+                snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+                //changes snack bar layout
+                @SuppressLint("RestrictedApi") Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                snackbarLayout.setPadding(0, 0, 0, 0);
+                //sets the button to have an x
+                helpButton.setImageResource(R.drawable.menu_help_exit_button);
+
+                //when behind is clicked it dismisses the snack bar
+                View behind = mySnackBar.findViewById(R.id.behind);
+                behind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                // add the custom snack bar layout to snack bar layout
+                snackbarLayout.addView(mySnackBar, 0);
+                snackbar.show();
+
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        helpButton.setImageResource(R.drawable.menu_help_button);
+                    }});
+            }
+        });
+
+        // menu snack bar
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // makes a snack bar with no text
+                final Snackbar snackbar = Snackbar.make(v, "", Snackbar.LENGTH_INDEFINITE);
+                // set the background to my snack bar
+                View mySnackbar = getLayoutInflater().inflate(R.layout.menu_snackbar, null);
+                //makes background transparent so the custom view can be seen
+                snackbar.getView().setBackgroundColor(Color.TRANSPARENT);
+                //changes snack bar layout
+                @SuppressLint("RestrictedApi") Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
+                snackbarLayout.setPadding(0, 0, 0, 0);
+                menuButton.setImageResource(R.drawable.menu_bars_exit_button);
+
+                // behind button
+                View behind = mySnackbar.findViewById(R.id.behind);
+                behind.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                // back button
+                View back = mySnackbar.findViewById(R.id.returnButton);
+                back.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+                // submit button
+                View submit = mySnackbar.findViewById(R.id.submitButton);
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+                // add the custom snack bar layout to snack bar layout
+                snackbarLayout.addView(mySnackbar, 0);
+                snackbar.show();
+
+                snackbar.addCallback(new Snackbar.Callback(){
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        menuButton.setImageResource(R.drawable.menu_bars_button);
+                    }});
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.replaceFrame, fragment);
+        fragmentTransaction.commit();
     }
 }
