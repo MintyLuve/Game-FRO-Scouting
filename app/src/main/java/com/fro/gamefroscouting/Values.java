d6214a1cde14bf072bf8ec728e1f67db09c78305
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-02-14 08:40:57 -0500

    added values

--- app/src/main/java/com/fro/gamefroscouting/Values.java
+++ app/src/main/java/com/fro/gamefroscouting/Values.java
@@ -1,4 +1,20 @@
 package com.fro.gamefroscouting;
 
 public class Values {
+    //start
+    public static int start_team_num, start_match_num, start_human_pos;
+    public static String start_scout_name, start_robot_pos;
+    //auto
+    public static int auto_speaker, auto_amp, auto_trap;
+    public static boolean auto_leave_start;
+    //teleop
+    public static int tele_aSpeaker, tele_nSpeaker, tele_amp, tele_trap;
+    public static boolean tele_ground_pickup, tele_station_pickup, tele_bonus;
+    //end game
+    public static int eg_made, eg_missed, eg_end_pos, eg_climb_type;
+    public static boolean eg_spot_left, eg_spot_center, eg_spot_right, eg_buddy_climb;
+    //notes
+    public static String notes_type_box;
+    public static int notes_defends, notes_defended;
+    public static boolean notes_robo_break, notes_robo_tip, notes_penalty;
 }
